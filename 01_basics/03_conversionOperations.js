b8b0d5ca23e7b890f48c5d55bec5870dbf01c87f
Shubhojeet <shubhojeet.ghosh@legittai.com>
2025-05-14 17:25:28 +0530

    conversion operations and rules

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -1,9 +1,9 @@
-let score = null;
+// let score = null;
 
 // console.log(score);
 // console.log(typeof score);
 
-let valueInNumber = Number(score);
+// let valueInNumber = Number(score);
 
 // console.log(valueInNumber);
 // console.log(typeof valueInNumber);
@@ -13,9 +13,9 @@ let valueInNumber = Number(score);
 //  true => 1
 //  false => 0
 
-let isLoggedIn = 0;
+// let isLoggedIn = 0;
 
-let booleanIsLoggedIn = Boolean(isLoggedIn);
+// let booleanIsLoggedIn = Boolean(isLoggedIn);
 
 // console.log(booleanIsLoggedIn);
 // console.log(typeof booleanIsLoggedIn);
@@ -24,10 +24,68 @@ let booleanIsLoggedIn = Boolean(isLoggedIn);
 // "" => false
 // "Shubh" => true
 
-let someNumber = {
-  name: "Shubh",
-};
-let stringNumber = String(someNumber);
+// let someNumber = 33;
 
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// let stringNumber = String(someNumber);
+
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// ******** Operations ********
+
+let value = 3;
+let negValue = -value;
+
+// console.log(negValue);
+
+// console.log(2 + 2);
+// console.log(2 ** 2);
+// console.log(2 ** 3);
+// console.log(6 / 3);
+// console.log(4 % 3);
+
+let str1 = "hello";
+let str2 = " Shubh";
+
+let str3 = str1 + str2;
+// console.log(str3);
+
+// console.log("1" + 2); // 12
+// console.log(1 + 2 + "2"); // 32
+
+// console(+true); // => 1
+// console.log(true+); error
+
+// console.log(+"");
+
+let num1, num2, num3;
+
+num1 = num2 = num3 = 2 + 2;
+
+// console.log(num1, num2);
+
+num1 = 8;
+
+// console.log(num1, num3);
+
+let gameCounter = 100;
+
+// console.log(gameCounter++);
+// console.log(gameCounter);
+
+let x = 3;
+const y = x++;
+
+console.log(`x:${x}, y:${y}`);
+// x:4, y:3
+
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);
+// a:4 b:4
+
+console.log("6" + 2); // converts the number into string in case of + operator
+console.log("6" - 2); // convertes the string to number, 4
+console.log("6" * 2); // convertes the string to number, 12
+console.log("6" / 2); // convertes the string to number, 3
