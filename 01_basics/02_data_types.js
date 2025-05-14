866921a48e177dd058ff82fdd2b34cf513c42d55
Shubhojeet <shubhojeet.ghosh@legittai.com>
2025-05-14 11:52:49 +0530

    data types and conversions

---
+++ 01_basics/02_data_types.js
@@ -0,0 +1,23 @@
+// treats all the codes declared as the newer version of JS, so many actions which where unsafe and are not good practices are now checked by Node js engine and are cautght as errors
+"use strict";
+
+let name = "Shubh";
+let age = 18;
+let isLoggedIn = false;
+
+// Primitive Data Types
+// number => 2 to the power 53
+// bigint => 123456789123456n
+//  string => "Hello"
+// boolean => true/false
+// null => standalone value
+// undefined => variable which is not yet defined but delared
+// symbol => for uniqueness
+
+// Non-Primitive (Reference) Data Types
+// object
+
+console.log(typeof isLoggedIn);
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,33 @@
+let score = null;
+
+// console.log(score);
+// console.log(typeof score);
+
+let valueInNumber = Number(score);
+
+// console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+//  true => 1
+//  false => 0
+
+let isLoggedIn = 0;
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+
+// console.log(booleanIsLoggedIn);
+// console.log(typeof booleanIsLoggedIn);
+
+//  1 = true; 0 => false
+// "" => false
+// "Shubh" => true
+
+let someNumber = {
+  name: "Shubh",
+};
+let stringNumber = String(someNumber);
+
+console.log(stringNumber);
+console.log(typeof stringNumber);
